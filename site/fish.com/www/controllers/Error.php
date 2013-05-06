491fa27cfd7310c1a3fcff61746fa3e3979939e1
techcms <techcms@outlook.com>
2013-05-06 18:13:47 +0800

    集成Preids

--- site/fish.com/www/controllers/Error.php
+++ site/fish.com/www/controllers/Error.php
@@ -7,12 +7,13 @@ class ErrorController extends \Sow\Sys\Control
 	}
 
 	public function indexAction() {
-		Y::dump($this->GET("pasge"));
+		//Y::dump($this->GET("pasge"));
 	}
 
-	public function errorAction() {
-		Y::dump($this->GET("pasge"));
+	public function errorAction( $exception ) {
+		Y::dump( $exception );
 	}
+
 	public function error404Action() {
 		// Y::dump(Y::app());
 		//Y::dump($this->GET("page",1),$this->GET("page",1),$this->GET("page",1));
