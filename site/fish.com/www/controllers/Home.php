32952654a3d028e2abb1b6c8549369102d4ad446
ohmyzi <ohmyzi@outlook.com>
2013-05-31 18:04:33 +0800

    delete httpful

--- site/fish.com/www/controllers/Home.php
+++ site/fish.com/www/controllers/Home.php
@@ -2,28 +2,44 @@
 use Sow\bug as Y;
 use Sow\util\FB as fb;
 use Sow\DB as DB;
-<<<<<<< HEAD
+
 use Sow\log\Monolog as log;
 use Guzzle\Http\Client;
 
 
-class HomeController extends \Sow\sys\Control
-=======
-use Guzzle\Http\Client;
 
 class Home_Controller extends \Sow\sys\Control
->>>>>>> refs/heads/master
 {
 	public function init() {
 		$this->setViewpath( Y::view( $this->getModuleName() ) );
 	}
 
 	public function indexAction() {
-<<<<<<< HEAD
-		$client = new Client('http://www.candou.com');
-		$request = $client->get('/');
-		$response = $request->send();
-		Y::dump($response);
+
+		$url = \Purl\Url::parse( 'http://jwage.com' )
+		->set( 'scheme', 'https' )
+		->set( 'port', '443' )
+		->set( 'user', 'jwage' )
+		->set( 'pass', 'password' )
+		->set( 'path', 'about/me' )
+		->set( 'query', 'param1=value1&param2=value2' )
+		->set( 'fragment', 'about/me?param1=value1&param2=value2' );
+
+		echo $url->getUrl();
+	}
+	public function demoAction() {
+		//Y::dump($this->GET('page'));
+
+		// fb::log( Y::config() );
+		// fb::log( Y::config()->toArray() );
+		// fb::info( 'Info Message' );
+		// fb::warn( 'Warn Message' );
+		// fb::error( 'Error Message' );
+		// $client = new Client('http://www.techweb.com.cn');
+		// $request = $client->get('/');
+		// $response = $request->send();
+		// Y::dump($response);
+
 		/*
 		const EMERGENCY = 'emergency';
     const ALERT     = 'alert';
@@ -37,20 +53,6 @@ class Home_Controller extends \Sow\sys\Control
 		// $log = log::stream('fish');
 		// $log->addDebug('123213213');
 		// $log->addWarning('123213213');
-=======
-		$client = new Client('https://api.github.com');
->>>>>>> refs/heads/master
-	}
-	public function demoAction() {
-		//Y::dump($this->GET('page'));
-
-		// fb::log( Y::config() );
-		// fb::log( Y::config()->toArray() );
-		// fb::info( 'Info Message' );
-		// fb::warn( 'Warn Message' );
-		// fb::error( 'Error Message' );
-
-
 		// $imagine = new Imagine\Gd\Imagine();
 		// $size = new Imagine\Image\Box( 200, 200 );
 
