dcdb75ab9e3548cfbf8f4d5aa91a1db48530a427
ohmyzi <ohmyzi@outlook.com>
2013-05-31 16:07:02 +0800

    delete test file

--- test/t.php
+++
@@ -1,6 +0,0 @@
-<?php
-$str = "@abcd@asddfasf@asfdasfas@bitch@dfasdas.字母com";
-$p = "/@[\x{4e00}-\x{9fa5}A-Za-z0-9_.]{2,16}[^@.]/u";
-preg_match_all($p,$str,$m);
-print_r($m);
-var_dump(htmlentities($str, ENT_COMPAT));
